f40c1de5e90c612cbf60dd8c1625b9b8eeb1fe73
alejandrodp <diaza913@gmail.com>
2022-03-25 13:05:41 -0600

    Mejora mesa de pruebas

--- exp1/code/test_alu.sv
+++ exp1/code/test_alu.sv
@@ -7,7 +7,11 @@ module test_alu;
 	logic [N-1:0] b;
 	logic [N-1:0] a_values[4];
 	logic [N-1:0] b_values[4];
-	logic [3:0] result_expected;
+	logic [N:0] result_expected;
+	logic c_expected;
+	logic z_expected;
+	logic n_expected;
+	logic v_expected;
 	logic c, z, n, v;
 	
 	alu #(.N(N)) alu_unit (
@@ -36,11 +40,22 @@ module test_alu;
 			b = b_values[i];
 			#10;
 			result_expected = (a * b);
+			if (opcode inside {4'b0110, 4'b1110})
+				c_expected = result_expected[N];
+			else
+				c_expected = 1'b0;
+			z_expected = result_expected == '0;
+			n_expected = result_expected[N-1];
+			
+			if (opcode === 4'b0110) v_expected = ~(a[N-1] ^ b[N-1]) & (result_expected[N-1] ^ b[N-1]);
+			else if (opcode === 4'b1110) v_expected = (a[N-1] ^ b[N-1]) & (result_expected[N-1] == b[N-1]);
+			else v_expected = 1'b0;
+			
 			assert (result === result_expected) else $display("Result expected: %b, result got it: %b", result_expected, result);
-			assert (c === 1'b0) else $display("Carry out expected: %b, got it: %b", 1'b0, c);
-			assert (z === (result_expected == '0)) else $display("Zero flag expected: %b, got it: %b", (result_expected == '0), z);
-			assert (n === result_expected[3]) else $display("Negative flag expected: %b, got it: %b", result_expected[3], n);
-			assert (v === 1'b0) else $display("Overflow flag expected: %b, got it: %b", result, v);
+			assert (c === c_expected) else $display("Carry out expected: %b, got it: %b", c_expected, c);
+			assert (z === z_expected) else $display("Zero flag expected: %b, got it: %b", z_expected, z);
+			assert (n === n_expected) else $display("Negative flag expected: %b, got it: %b", n_expected, n);
+			assert (v === v_expected) else $display("Overflow flag expected: %b, got it: %b", v_expected, v);
 		end
 	end
 	
